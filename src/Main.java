76788d23165d1af7c985a8de2e5631e990e76e4b
Viktoriaradchenko <cph-vr73@stud.ek.dk>
2025-11-10 23:37:17 +0100

    minor

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Chill Streamingtjeneste");
+        System.out.println("Vi laver en lille ændring");
     }
 }
